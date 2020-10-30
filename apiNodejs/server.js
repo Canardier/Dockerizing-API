4df69ea370444e21876230808a624701d43e315f
daffy <CuckOrDuck@hotmail.com>
2020-10-30 23:20:44 +0100

    add sequelize to server.js

--- apiNodejs/server.js
+++ apiNodejs/server.js
@@ -1,3 +1,5 @@
+const { Sequelize } = require('sequelize');
+const init = require("./migration/init-models");
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -15,6 +17,25 @@ app.get("/", (req, res) => {
 });
 
 require("./app/routes/customer.routes.js")(app);
+
+/*TODO for add sequelize connection*/
+async function connect() {
+  const sequelize = new Sequelize('mydb', 'root', 'root', {
+      host: 'localhost',
+      port: 3306,
+      dialect: 'mysql',
+  });
+  
+  try {
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+      await init.initModels(sequelize);
+      await sequelize.sync({}); //force:true to drop and create
+      console.log("All models were synchronized successfully.");
+  } catch (error) {
+      console.error('Unable to connect to the database:', error);
+  }
+}
 
 // set port, listen for requests
 app.listen(3000, () => {
